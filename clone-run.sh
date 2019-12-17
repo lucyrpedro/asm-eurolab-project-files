11894c5f140650d87abc7c89a7f9bc89f513b964
lucyrpedro <lu.ufmg@gmail.com>
2019-12-17 21:09:14 +0000

    Minor

--- clone-run.sh
+++ clone-run.sh
@@ -7,12 +7,14 @@ cd asm-eurolab-project-files/
 
 ./prepare.sh
 
-cp src/ior ../asm-eurolab-project-files/benchmarks
+# cp src/ior ../asm-eurolab-project-files/benchmarks
 # cp src/md-workbench ../../asm-eurolab-project-files/benchmarks (It's not working on the server!)
+cp /home/pedro/ior-3.2.1/build/src/ior asm-eurolab-project-files/benchmarks
 cp /home/pedro/md-workbench/build/src/md-workbench asm-eurolab-project-files/benchmarks
 
 ## Cleaning ###
-rm -rf ior*
-rm -rf md*
+
+# rm -rf ior*
+# rm -rf md*
 
 ./run.sh clean
