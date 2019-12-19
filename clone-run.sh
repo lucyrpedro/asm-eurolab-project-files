bfe6351f86952bf117e9054f4b85d514958dc920
lucyrpedro <lu.ufmg@gmail.com>
2019-12-19 18:32:18 +0000

    Minor

--- clone-run.sh
+++ clone-run.sh
@@ -2,31 +2,14 @@
 
 # Bash Script for cloning the files and run the tests
 
-# Options: Parameter $1
-
-# test      small parameters (short run)
-# none      real parameters (long run)
+# Local machine, small parameters
 
 git clone https://github.com/lucyrpedro/asm-eurolab-project-files.git
 
-## Running the tests ###
 cd asm-eurolab-project-files/
 
 ./prepare.sh
 
-# cp src/ior ../asm-eurolab-project-files/benchmarks
-# cp src/md-workbench ../../asm-eurolab-project-files/benchmarks (It's not working on the server!)
-cp /home/pedro/ior-3.2.1/build/src/ior asm-eurolab-project-files/benchmarks
-cp /home/pedro/md-workbench/build/src/md-workbench asm-eurolab-project-files/benchmarks
+## Running the tests ###
 
-## Cleaning ###
-
-# rm -rf ior*
-# rm -rf md*
-
-if [ $1 == 'test' ]
-then
-  ./run.sh clean $1
-else
-  ./run.sh clean none
-fi
+./run.sh clean test
