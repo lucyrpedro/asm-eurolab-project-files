afacb8e3a0bd2421fed15b662f97ddbce0a40228
lucyrpedro <lu.ufmg@gmail.com>
2019-12-20 16:59:05 +0000

    Minor

--- bash-scripts/run-dd.sh
+++ bash-scripts/run-dd.sh
@@ -100,7 +100,7 @@ function run_file(){
   fi
 }
 
-for i in {1..1}; do      # 10
+for i in {1..10}; do      # 10
   for j in "${blocksize_vec[@]}"; do     # 7
     for k in "${filesize_vec[@]}"; do   # 2
       run_file $i $j $k
