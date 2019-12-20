f221ae5ce54934b7cb2938df730962b1285c0769
lucyrpedro <lu.ufmg@gmail.com>
2019-12-20 22:02:58 +0000

    Minor

--- bash-scripts/run-dd.sh
+++ bash-scripts/run-dd.sh
@@ -50,9 +50,9 @@ if [ $dir == 'tmpfs' ]
 then test_dir=/dev/shm/testfile
 fi
 
-if [ $dir == 'fuse' ]
-  if [ $filter == 'passthrough_hp' ]
-    then test_dir=mnt-fuse/testfile
+if [ $dir == 'fuse' ]; then
+  if [ $filter == 'passthrough_hp' ]; then
+    test_dir=mnt-fuse/testfile
   else test_dir=mnt-fuse/dev/shm/testfile
   fi
 fi
